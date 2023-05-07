d897675b1da0534abd856ccad54ca51ea3a90bb8
rosefarts <gonesbird@gmail.com>
2023-05-07 15:47:24 +0200

    layers

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,194 @@
-use bevy::prelude::*;
+#![feature(drain_filter)]
+
+use std::ops::Mul;
+
+use bevy::{prelude::*, utils::HashMap};
+
+pub mod command;
+pub mod layer;
+pub mod timeline;
+
+#[derive(Component, Default)] // TODO: implament Default manually
+pub struct Layer {
+    frames: HashMap<i32, Handle<Image>>,
+    width: u32,
+    height: u32,
+}
+
+impl Layer {
+    pub fn new(width: u32, height: u32) -> Self {
+        Layer {
+            frames: HashMap::new(),
+            width,
+            height,
+        }
+    }
+
+    /// * clear - the clear color
+    pub fn insert_new_frame(
+        &mut self,
+        frame: i32,
+        clear: Color,
+        images: &mut Assets<Image>,
+    ) -> Option<Handle<Image>> {
+        if self.frames.contains_key(&frame) {
+            return None;
+        }
+        let image = images.add(Image {
+            data: (0..(self.height * self.width))
+                .map(|_| clear.as_rgba_u8())
+                .collect::<Vec<[u8; 4]>>()
+                .concat(),
+            ..default_image(self.width, self.height)
+        });
+        self.insert_frame(frame, image)
+    }
+
+    pub fn insert_frame(&mut self, frame: i32, image: Handle<Image>) -> Option<Handle<Image>> {
+        self.frames.insert(frame, Handle::clone(&image));
+        Some(image)
+    }
+
+    pub fn frame(&self, frame: i32) -> Option<&Handle<Image>> {
+        self.frames.get(&frame)
+    }
+}
+
+#[derive(Bundle, Default)]
+pub struct LayerBundle {
+    layer: Layer,
+    transform: Transform,
+    sprite: Sprite,
+    texture: Handle<Image>,
+    global_transform: GlobalTransform,
+    visibility: Visibility,
+    computed_visibility: ComputedVisibility,
+}
+
+#[derive(Resource)]
+pub struct Timeline {
+    layers: Vec<Entity>,
+    width: u32,
+    height: u32,
+    frame: i32,
+}
+
+impl Timeline {
+    pub fn new(width: u32, height: u32) -> Self {
+        Self {
+            layers: Vec::new(),
+            width,
+            height,
+            frame: 0,
+        }
+    }
+
+    pub fn layers(&self) -> &Vec<Entity> {
+        &self.layers
+    }
+
+    pub fn layers_change_this_name<'a>(
+        &'a self,
+        layers: &'a Query<(Entity, &'a Layer)>,
+    ) -> impl Iterator<Item = (Entity, &'a Layer)> {
+        layers.iter().filter(|(e, _)| self.layers.contains(e))
+    }
+
+    pub fn add_new_layer(
+        &mut self,
+        clear: Color,
+        commands: &mut Commands,
+        images: &mut Assets<Image>,
+    ) -> Entity {
+        let mut layer = Layer::new(self.width, self.height);
+        let image = layer.insert_new_frame(self.frame, clear, images).unwrap();
+        self.add_layer(image, commands)
+    }
+
+    pub fn add_layer(&mut self, image: Handle<Image>, commands: &mut Commands) -> Entity {
+        let mut layer = Layer::new(self.width, self.height);
+        layer.insert_frame(self.frame, Handle::clone(&image));
+
+        let entity = commands
+            .spawn(LayerBundle {
+                layer,
+                texture: image,
+                transform: Transform::from_translation(Vec3::new(
+                    0.0,
+                    0.0,
+                    self.layers.len() as f32 + 1.0,
+                )),
+                ..Default::default()
+            })
+            .id();
+        self.layers.push(entity);
+        entity
+    }
+
+    pub fn remove_layer(&mut self, layer: Entity, commands: &mut Commands) -> bool {
+        if let Some(entity_commands) = commands.get_entity(layer) {
+            entity_commands.despawn_recursive();
+        }
+        self.layers.drain_filter(|l| *l == layer).next().is_some()
+    }
+}
+
+pub fn default_image(width: u32, height: u32) -> Image {
+    use bevy::render::{
+        render_resource::{
+            Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+        },
+        texture::ImageSampler,
+    };
+    Image {
+        data: vec![255; (width * height * 4) as usize],
+        sampler_descriptor: ImageSampler::nearest(),
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size: Extent3d {
+                width,
+                height,
+                depth_or_array_layers: 1,
+            },
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Rgba8UnormSrgb,
+            usage: TextureUsages::COPY_DST | TextureUsages::TEXTURE_BINDING,
+            view_formats: &[],
+        },
+        texture_view_descriptor: None,
+    }
+}
+
+pub trait Draw {
+    fn draw(&mut self, pos: Vec2, color: Color);
+    fn try_draw(&mut self, pos: Vec2, color: Color) -> bool;
+}
+
+impl Draw for Image {
+    /// ## Panics
+    /// if the position is outside the bounds
+    fn draw(&mut self, pos: Vec2, color: Color) {
+        let i = pos.x as u32 + pos.y as u32 * self.size().x as u32;
+
+        let i = i as usize * 4;
+
+        let color = color.as_rgba_u8();
+
+        self.data[i] = color[0];
+        self.data[i + 1] = color[1];
+        self.data[i + 2] = color[2];
+        self.data[i + 3] = color[3];
+    }
+    /// ## Returns
+    /// `true` if drawing succeeded, `false` otherwise.
+    fn try_draw(&mut self, pos: Vec2, color: Color) -> bool {
+        if (0.0..self.size().x).contains(&pos.x) && (0.0..self.size().y).contains(&pos.y) {
+            self.draw(pos, color);
+            true
+        } else {
+            false
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-#![feature(generic_const_exprs, array_chunks)]
+#![feature(generic_const_exprs, array_chunks, exclusive_range_pattern)]
+
+use core::time;
 
 use bevy::{
     input::mouse::{MouseMotion, MouseWheel},
@@ -11,10 +13,12 @@ use bevy::{
         texture::{BevyDefault, ImageSampler, ImageType, TextureFormatPixelInfo},
     },
 };
+use sprinkle::{Draw, Layer, Timeline};
 
 fn main() {
     fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
         const WIDTH: usize = 32;
+        const HEIGHT: usize = 32;
 
         // spawn camera
         commands.spawn(Camera2dBundle {
@@ -26,30 +30,45 @@ fn main() {
         });
 
         // create texture
-        let img = image_function::<WIDTH, 16>(|x, y| [x / 32.0, y / 16.0, 0.0, 1.0]);
+        let img =
+            image_function::<WIDTH, HEIGHT>(|x, y| [x / WIDTH as f32, y / HEIGHT as f32, 0.0, 1.0]);
         let image = images.add(img);
 
-        commands.spawn(SpriteBundle {
-            transform: Transform::from_scale(Vec3::splat(1.0)),
-            texture: Handle::clone(&image),
-            ..Default::default()
-        });
+        let mut timeline = Timeline::new(WIDTH as u32, HEIGHT as u32);
+
+        let mut canvas = Canvas {
+            layer_id: None,
+            cursor_position: Err(Vec2::ZERO),
+            layer_index: 0,
+            size: Vec2::new(WIDTH as f32, HEIGHT as f32),
+        };
+
+        canvas.layer_id = Some(timeline.add_layer(image, &mut commands));
+
+        commands.insert_resource(timeline);
+        commands.insert_resource(canvas);
 
         // spawn cursor
         commands.spawn((
             SpriteBundle {
                 transform: Transform::from_scale(Vec3::splat(1.0))
-                    .with_translation(Vec3::new(0.0, 0.0, 10.0)),
+                    .with_translation(Vec3::new(0.0, 0.0, 100.0)),
                 ..Default::default()
             },
             Cursor,
         ));
 
-        commands.insert_resource(Canvas {
-            image,
-            cursor_position: Err(Vec2::ZERO),
-            size: Vec2::new(WIDTH as f32, 16.0),
-        });
+        commands.spawn((
+            SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgba(0.5, 0.5, 0.5, 0.5),
+                    ..Default::default()
+                },
+                transform: Transform::from_scale(Vec3::new(WIDTH as f32, HEIGHT as f32, 1.0)),
+                ..Default::default()
+            },
+            Background,
+        ));
     }
 
     fn zoom_camera(
@@ -89,7 +108,8 @@ fn main() {
     fn move_cursor(
         window: Query<&Window>,
         camera: Query<(&Transform, &OrthographicProjection), Without<Cursor>>,
-        mut cursor: Query<(&mut Transform, &mut Sprite), With<Cursor>>,
+        layers: Query<&Transform, With<Layer>>,
+        mut cursor: Query<(&mut Transform, &mut Sprite), (With<Cursor>, Without<Layer>)>,
         mut canvas: ResMut<Canvas>,
     ) {
         let (mut cursor_pos, mut sprite) = cursor.single_mut();
@@ -99,23 +119,29 @@ fn main() {
         let (cam_trans, proj) = camera.single();
 
         if let Some(mut pos) = window.cursor_position() {
-            let offset = proj.area.max + cam_trans.translation.xy() * Vec2::new(-1.0, 1.0);
+            let canvas_offset = canvas
+                .layer_id
+                .map(|id| layers.get(id).unwrap().translation.xy())
+                .unwrap_or(Vec2::ZERO);
+
+            let cam_offset = cam_trans.translation.xy();
+
+            let offset = proj.area.max + cam_offset * Vec2::new(-1.0, 1.0);
+
             pos *= proj.scale;
             pos -= offset;
 
             {
-                let pos = pos + canvas.size / 2.0;
+                let pos = pos + canvas.size / 2.0 + canvas_offset * Vec2::new(-1.0, 1.0);
                 let (x, y, w, h) = (pos.x, pos.y, canvas.size.x, canvas.size.y);
 
-                let i = pos.x as u32 + pos.y as u32 * canvas.size.x as u32;
-
-                println!("x: {}, y: {} i: {}", x, y, i);
+                // println!("x: {}, y: {}", x, y);
 
                 if (0.0..w).contains(&x) && (0.0..h).contains(&y) {
-                    canvas.cursor_position = Ok(pos);
+                    canvas.cursor_position = Ok(pos - canvas_offset);
                     sprite.color = Color::WHITE;
                 } else {
-                    canvas.cursor_position = Err(pos);
+                    canvas.cursor_position = Err(pos - canvas_offset);
                     sprite.color = Color::RED;
                 }
             }
@@ -130,26 +156,114 @@ fn main() {
     fn paint(
         canvas: Res<Canvas>,
         input: Res<Input<MouseButton>>,
+        layers: Query<(&Layer, &Transform)>,
         mut images: ResMut<Assets<Image>>,
     ) {
         if input.pressed(MouseButton::Left) {
-            let image = images.get_mut(&canvas.image).unwrap();
+            // let image = images.get_mut(&canvas.image).unwrap();
+            let frame_index = 0;
+
+            let mut offset = Vec2::ZERO; // and this
 
             // TODO: only draw when cursor moved
-            if let Ok(pos) = canvas.cursor_position {
-                let i = pos.x as u32 + pos.y as u32 * canvas.size.x as u32;
-                let i = i as usize * 4;
 
-                image.data[i] = 255;
-                image.data[i + 1] = 255;
-                image.data[i + 2] = 255;
+            if let Ok(pos) = canvas.cursor_position {
+                let Some(image) = canvas
+                    .layer_id
+                    .and_then(|id| {
+                        layers.get(id).ok().and_then(|(layer, trans)| {
+                            offset = trans.translation.xy();
+                            layer
+                                .frame(frame_index)
+                                .and_then(|frame| images.get_mut(frame))
+                        })
+                    }) else {
+                        warn!("Something went wrong while trying to draw");
+                        return
+                    };
+
+                image.draw(
+                    pos + offset, // TODO: remove this
+                    match canvas.layer_index % 4 {
+                        0 => Color::ALICE_BLUE,
+                        1 => Color::ANTIQUE_WHITE,
+                        2 => Color::AQUAMARINE,
+                        3 => Color::MAROON,
+                        _ => unreachable!(),
+                    },
+                );
             }
         }
+    }
+
+    fn add_image(
+        mut commands: Commands,
+        mut canvas: ResMut<Canvas>,
+        mut timeline: ResMut<Timeline>,
+        mut images: ResMut<Assets<Image>>,
+        input: Res<Input<KeyCode>>,
+    ) {
+        if !input.just_pressed(KeyCode::N) {
+            return;
+        }
+
+        println!("!!! addded image !!!");
+        canvas.layer_id = Some(timeline.add_new_layer(
+            Color::rgba(0.0, 0.0, 0.0, 0.0),
+            &mut commands,
+            &mut images,
+        ));
+        canvas.layer_index = timeline.layers().len() as i32 - 1;
+        canvas.layer_id = Some(timeline.layers()[canvas.layer_index as usize]);
+    }
+
+    fn change_layer(
+        mut canvas: ResMut<Canvas>,
+        timeline: Res<Timeline>,
+        input: Res<Input<KeyCode>>,
+    ) {
+        if input.just_pressed(KeyCode::Up) {
+            canvas.layer_index += 1;
+        } else if input.just_pressed(KeyCode::Down) {
+            canvas.layer_index -= 1;
+        } else {
+            return;
+        }
+
+        let layer_count = timeline.layers().len() as i32;
+
+        match canvas.layer_index {
+            ..0 => canvas.layer_index = layer_count - 1,
+            1.. if layer_count != 0 => canvas.layer_index %= layer_count,
+            _ => (),
+        }
+        if layer_count != 0 {
+            canvas.layer_index %= layer_count;
+            canvas.layer_id = Some(timeline.layers()[canvas.layer_index as usize]);
+        }
+
+        println!("changed layer to {}", canvas.layer_index);
+    }
+
+    fn paralax(
+        mut query: Query<&mut Transform, (Without<Camera2d>, Without<Cursor>)>,
+        cam: Query<&Transform, With<Camera2d>>,
+    ) {
+        let Transform {
+            translation: cam_pos,
+            ..
+        } = cam.single();
+
+        query.for_each_mut(|mut trans| {
+            let pos = trans.translation;
+            trans.translation = (cam_pos.xy() * pos.z * 0.1).extend(pos.z)
+        });
     }
 
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, (add_image, change_layer, paralax))
         .add_systems(PreUpdate, (zoom_camera, move_camera))
         .add_systems(
             PostUpdate,
@@ -203,8 +317,13 @@ where
 struct Canvas {
     size: Vec2,
     cursor_position: Result<Vec2, Vec2>,
-    image: Handle<Image>,
+    layer_id: Option<Entity>,
+    layer_index: i32,
+    // image: Handle<Image>,
 }
 
 #[derive(Component)]
 struct Cursor;
+
+#[derive(Component)]
+struct Background;
