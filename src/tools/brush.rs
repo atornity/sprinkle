409faa10a0cca05602dd7f6133f45181d976080a
rosefarts <gonesbird@gmail.com>
2023-05-09 21:53:14 +0200

    brush iterpolation
    still just linear but it looks alright

--- src/tools/brush.rs
+++ src/tools/brush.rs
@@ -6,7 +6,7 @@ use super::*;
 pub struct BrushState {
     pub data: Option<Vec<u8>>,
     pub color: Color,
-    pub last_position: Vec2,
+    pub last_position: Option<Vec2>,
 }
 
 pub fn start_painting(
@@ -23,14 +23,15 @@ pub fn start_painting(
     brush.data = Some(image.data.clone());
 }
 
-pub fn stop_painting(mut brush_state: ResMut<BrushState>, mut history: ResMut<History>) {
+pub fn stop_painting(mut brush: ResMut<BrushState>, mut history: ResMut<History>) {
     info!("stopped painting!");
 
-    history.add(HistoryItem::Painted(brush_state.data.take().unwrap()));
+    history.add(HistoryItem::Painted(brush.data.take().unwrap()));
+    brush.last_position = None;
 }
 
 pub fn paint(
-    brush: Res<BrushState>,
+    mut brush: ResMut<BrushState>,
     canvas: Res<Canvas>,
     layers: Query<&Layer>,
     mut images: ResMut<Assets<Image>>,
@@ -38,7 +39,17 @@ pub fn paint(
     let layer = layers.get(canvas.layer_id).unwrap();
     let image = images.get_mut(&layer.frames[&0]).unwrap();
 
-    if let Ok(pos) = canvas.cursor_position {
-        image.paint(pos, brush.color);
+    if let Ok(next_pos) = canvas.cursor_position {
+        if let Some(mut pos) = brush.last_position {
+            let dir = (next_pos - pos).clamp_length_max(1.0);
+            let len = dir.length();
+
+            while pos.distance_squared(next_pos) > len {
+                pos += dir;
+                image.paint(pos, brush.color);
+            }
+        }
+        image.paint(next_pos, brush.color);
+        brush.last_position = Some(next_pos);
     }
 }
